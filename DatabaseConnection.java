eb54b8f14cff7ffa1e780d9cdc6f2c3f03026dd9
Arturocursos <arturocursoscom@gmail.com>
2024-10-31 17:07:51 -0500

    Update DatabaseConnection.java

--- DatabaseConnection.java
+++ DatabaseConnection.java
@@ -6,8 +6,8 @@ import java.sql.SQLException;
 
 public class DatabaseConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/your_database_name"; // Cambia el nombre de la base de datos
-    private static final String USER = "i7313497_c9gm1";
-    private static final String PASSWORD = "W.s7k1wlloywMlwn0Y851";
+    private static final String USER = "******";
+    private static final String PASSWORD = "*******";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
